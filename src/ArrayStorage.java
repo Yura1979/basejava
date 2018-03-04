d7766cf81ea07975b64ebcb5321606ee0091c58a
Yura1979 <tyi@outlook.com>
2018-03-04 09:56:29 +0200

    resolved

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                delete(storage[i].uuid);
+            }
+        }
     }
 
     void save(Resume r) {
+        int index = 0;
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                index = i;
+                break;
+            }
+        }
+
+        if (r != null) {
+            storage[index] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+//            System.out.println(storage[i].uuid);
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newResume = new Resume[size()];
+        int index = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                //System.out.println(resume);
+                newResume[index++] = resume;
+            }
+        }
+        return newResume;
     }
 
     int size() {
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -13,6 +13,7 @@ public class MainTestArrayStorage {
         r3.uuid = "uuid3";
 
         ARRAY_STORAGE.save(r1);
+//        System.out.println(ARRAY_STORAGE.get("uuid1"));
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
