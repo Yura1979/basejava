19d9254086dfa3d1ca45178a1bc00d7712a87c0d
Yura1979 <tyi@outlook.com>
2018-03-04 14:13:13 +0200

    recode with Arrays class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,29 +1,44 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    //Resume[] storage = new Resume[10000];
+    Resume[] storage;
+    int currentElement;
+
+    public ArrayStorage() {
+        this.storage = new Resume[10000];
+        this.currentElement = 0;
+    }
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                delete(storage[i].uuid);
-            }
-        }
+//        for (int i = 0; i < storage.length; i++) {
+//            if (storage[i] != null) {
+//                delete(storage[i].uuid);
+//            }
+//        }
+        Arrays.fill(storage, null);
+        this.currentElement = 0;
     }
 
     void save(Resume r) {
-        int index = 0;
-
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                index = i;
-                break;
-            }
-        }
-
-        if (r != null) {
-            storage[index] = r;
+//        int index = 0;
+//
+//        for (int i = 0; i < storage.length; i++) {
+//            if (storage[i] == null) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        if (r != null) {
+//            storage[index] = r;
+//        }
+        if (r != null){
+            storage[currentElement++] = r;
+            System.out.println("Saving " + r);
         }
     }
 
@@ -39,25 +54,28 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].uuid == uuid) {
-                storage[i] = null;
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                System.out.println("we deleting " + storage[i]);
+                storage[i] = storage[--currentElement];
             }
         }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] newResume = new Resume[size()];
-        int index = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                //System.out.println(resume);
-                newResume[index++] = resume;
-            }
-        }
-        return newResume;
+//        Resume[] newResume = new Resume[size()];
+//        int index = 0;
+//        for (Resume resume : storage) {
+//            if (resume != null) {
+//                //System.out.println(resume);
+//                newResume[index++] = resume;
+//            }
+//        }
+//        return newResume;
+        return Arrays.copyOfRange(storage, 0, currentElement);
     }
 
     int size() {
